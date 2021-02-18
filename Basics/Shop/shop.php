d1111bc6bf359d86c69b87ea6b4f385367dbf4ad
isilnieks <isilnieks@gmail.com>
2021-02-18 14:46:00 +0200

    First exercise

---
+++ Basics/Shop/shop.php
@@ -0,0 +1,27 @@
+<?php
+$shop = [
+    'bread' => 1,
+    'milk' => 2,
+    'hat' => 10,
+    'water' => 3,
+    'shoes' => 20,
+];
+
+function total(string $product, int $amount, array $shop): string
+{
+    foreach ($shop as $key => $value) {
+        if ($product == $key) {
+            $fullPrice = $shop[$key] * $amount;
+
+            return 'Your order is to buy ' . $amount . ' ' . $key . '.' . ' Price : ' . $fullPrice . 'euros';
+        }
+    }
+    return ':(';
+}
+
+
+$product = readline('Bread = 1. Milk = 2. Hat = 10. Water = 3. Shoes = 20 . Enter the product you would like to buy: ');
+$amount = readline('Enter the amount: ');
+echo total($product, $amount, $shop);
+
+
