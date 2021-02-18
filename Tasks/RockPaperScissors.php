ae5deb0bcec9eb46920cc7ff2dd5d2f8c355bda5
isilnieks <isilnieks@gmail.com>
2021-02-18 13:30:28 +0200

    Rock paper scissors

--- Tasks/RockPaperScissors.php
+++ Tasks/RockPaperScissors.php
@@ -1,9 +1,34 @@
 <?php
 
-$options = ['rock', 'paper', 'scissors'];
+$pcOptions = ['R', 'P', 'S'];
+$playerChoice = strtoupper(readline('Enter (R)ock, (P]aper or (S)cissors: '));
+
+$pcRandom = $pcOptions[rand(0,2)];
+
+if(! in_array($playerChoice, $pcOptions))
+{
+    echo "Invalid option!\n";
+    exit;
+}
+echo 'Computer: ' . $pcRandom . PHP_EOL;
+echo 'You: ' . $playerChoice . PHP_EOL;
+if($playerChoice === $pcRandom){
+
+    echo "Its a tie\n";
+}
+switch([$playerChoice, $pcRandom]){
+    case['P' , 'R']:
+    case['R' , 'S']:
+    case['S' , 'P']:
+        echo "You win!\n";
+        Break;
+    case['P' , 'S']:
+    case['R' , 'P']:
+    case['S' , 'R']:
+        echo "You lose!\n";
+        Break;
+
+}
 
 
 
-
-
-$player = readline('Enter ')
