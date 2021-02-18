b5f80cd576072fa325cdb06d84478857344d1682
isilnieks <isilnieks@gmail.com>
2021-02-18 12:00:05 +0200

    Second commit

--- Tasks/sheep.php
+++ Tasks/sheep.php
@@ -1,1 +1,12 @@
 <?php
+$animals = ['sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'wolf', 'sheep', 'sheep'];
+
+for ($i = 0; $i < count($animals) - 1; $i++) {
+    if ($animals[$i] === 'wolf') {
+        echo 'HEHE ';
+    } elseif ($animals[$i + 1] === 'wolf') {
+        echo 'OMG ';
+    } else {
+        echo 'Happy ';
+    }
+}
