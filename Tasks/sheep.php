e95b91bb2b84604ebfe483b597e6c5dcf12a5237
isilnieks <isilnieks@gmail.com>
2021-02-22 10:34:00 +0200

    Sheep fixed

--- Tasks/sheep.php
+++ Tasks/sheep.php
@@ -4,7 +4,7 @@ $animals = ['sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'wolf', 'sheep', 'sheep'
 for ($i = 0; $i < count($animals) - 1; $i++) {
     if ($animals[$i] === 'wolf') {
         echo 'HEHE ';
-    } elseif ($animals[$i] === 'sheep' && $animals[$i + 1] === 'wolf') {
+    } elseif ($animals[$i] === 'sheep' && $animals[$i + 1] === 'wolf' || $animals[$i] === 'sheep' && $animals[$i - 1] === 'wolf') {
         echo 'OMG ';
     } else {
         echo 'Happy ';
