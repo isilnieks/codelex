3a3b57ae024f61e97948d34ce01356abaf861679
isilnieks <isilnieks@gmail.com>
2021-02-25 18:02:43 +0200

    CoffeeMachine

--- Tasks/Complete.php
+++ Tasks/Complete.php
@@ -1,4 +1,18 @@
 <?php
+$menu = [
+    1 => [
+        'name' => 'Latte',
+        'price' => 190
+    ],
+    2 => [
+        'name' => 'Cappuccino',
+        'price' => 230
+    ],
+    3 => [
+        'name' => 'Black Coffee',
+        'price' => 203
+    ],
+];
 $money = [1 => 10, 2 => 5, 5 => 3, 10 => 4, 20 => 2, 50 => 2, 100 => 2, 200 => 1];
 
 function moneyCount(array $money): int
@@ -14,7 +28,20 @@ $wallet = moneyCount($money);
 
 echo 'You have ' . $wallet . ' coins' . PHP_EOL;
 
-$price = 200;
+
+for ($i = 1; $i < count($menu) + 1; $i++) {
+    echo $i . ' ' . $menu[$i]['name'] . ' ' . number_format($menu[$i]['price'] / 100, 2) . '$' . PHP_EOL;
+}
+
+do {
+    $choice = readline('Choose your product:');
+    if ($choice < 1 || $choice > 3) {
+        echo 'There is no such product. Try again.', PHP_EOL;
+    }
+} while ($choice < 1 || $choice > count($menu));
+
+$price = $menu[$choice]['price'];
+
 $coinsEntered = [];
 
 while ($price > array_sum($coinsEntered)) {
@@ -48,6 +75,7 @@ while ($price > array_sum($coinsEntered)) {
         echo 'Invalid coin or insufficient amount of coins ' . PHP_EOL;
     }
 }
+
 $change = array_sum($coinsEntered) - $price;
 
 function isThereChange(array $coinsEntered, int $price, int $change): string
@@ -60,4 +88,24 @@ function isThereChange(array $coinsEntered, int $price, int $change): string
 }
 
 echo isThereChange($coinsEntered, $price, $change);
+
+if ($change > 0) {
+    foreach (array_reverse($money, true) as $key => $value) {
+        if ($key <= $change) {
+            echo 'Returned coin: ' . $key . PHP_EOL;
+            $money[$key] = $money[$key] + 1;
+            $change -= $key;
+        }
+    }
+}
+$wallet = moneyCount($money);
+
+echo $wallet . ' coins left in your wallet' . PHP_EOL;
+
+
+
+
+
+
+
 
